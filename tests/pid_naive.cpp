b4aef847c31734b8e625e2272acb13b2af3c027e
Stefan Hagen <stefan@hagen.link>
2020-12-27 22:35:21 +0100

    Added test for lambda version of naive non-reverting PID controller.

--- tests/pid_naive.cpp
+++ tests/pid_naive.cpp
@@ -15,3 +15,10 @@ TEST_CASE("naive PID controller from example with target value remains stable.",
     reading read{3.14156}, target{read};
     REQUIRE(controller.calculate(target, read) == reading{0});
 }
+
+TEST_CASE("naive PID controller as lambda from example is constructable.", "[positive]") {
+    using reading = double;
+    reading d_t = .1, lo = -21., hi = +21., k_p = .1, k_i = .5, k_d = .01;
+    auto controller = pid::controller<reading>(d_t, lo, hi, k_p, k_i, k_d);
+    REQUIRE(controller(reading{0}, reading{3.14156}, reading{0}) < reading{0});
+}
