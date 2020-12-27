2716a701a5285fb36620063b21b84aef646645b9
Stefan Hagen <stefan@hagen.link>
2020-12-27 22:39:36 +0100

    Added formal test for inverting controller lambda.

--- tests/pid_naive.cpp
+++ tests/pid_naive.cpp
@@ -22,3 +22,10 @@ TEST_CASE("naive PID controller as lambda from example is constructable.", "[pos
     auto controller = pid::controller<reading>(d_t, lo, hi, k_p, k_i, k_d);
     REQUIRE(controller(reading{0}, reading{3.14156}, reading{0}) < reading{0});
 }
+
+TEST_CASE("naive inverting PID controller as lambda from example is constructable.", "[positive]") {
+    using reading = double;
+    reading d_t = .1, lo = -21., hi = +21., k_p = .1, k_i = .5, k_d = .01;
+    auto controller = pid::controller_inverting<reading>(d_t, lo, hi, k_p, k_i, k_d);
+    REQUIRE(controller(reading{0}, reading{3.14156}, reading{0}) > reading{0});
+}
