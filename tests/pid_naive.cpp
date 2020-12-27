f47db36b0a91e9c9767f99666081de18fc4fd9c7
Stefan Hagen <stefan@hagen.link>
2020-12-27 19:41:44 +0100

    Added no-op controller test case.

--- tests/pid_naive.cpp
+++ tests/pid_naive.cpp
@@ -7,3 +7,11 @@ TEST_CASE("naive PID controller from example is constructable.", "[positive]") {
     auto controller = pid::naive<reading>(d_t, lo, hi, k_p, k_i, k_d);
     REQUIRE(controller.calculate(reading{0}, reading{3.14156}, reading{0}) < reading{0});
 }
+
+TEST_CASE("naive PID controller from example with target value remains stable.", "[positive]") {
+    using reading = double;
+    reading d_t = .1, lo = -21., hi = +21., k_p = .1, k_i = .5, k_d = .01;
+    auto controller = pid::naive<reading>(d_t, lo, hi, k_p, k_i, k_d);
+    reading read{3.14156}, target{read};
+    REQUIRE(controller.calculate(target, read) == reading{0});
+}
